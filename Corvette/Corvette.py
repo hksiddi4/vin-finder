3f0ebe3139715f019819349815e266d96be6ff2c
hksiddi4 <hussainks92@gmail.com>
2024-08-16 20:10:59 -0500

    Added Corvette/Finding ZR1

---
+++ Corvette/Corvette.py
@@ -0,0 +1,232 @@
+import fitz
+import csv
+import json
+import requests
+import time
+from variables import *
+
+# Extract text from PDF -------------------------------------------------------------------------
+def extractPDF(contentsGet, updated_vin):
+    try:
+        with open(f'{year}/temp.pdf', "wb") as f:
+            f.write(contentsGet.content)
+        doc = fitz.open(f'{year}/temp.pdf')
+        text = ""
+        if len(doc) > 0:
+            if len(doc) > 1:
+                with open(f"{year}/notes.txt", "a") as nf:
+                    nf.write(f"{updated_vin} - Multiple Pages\n")
+            page = doc.load_page(0)
+            text = page.get_text()
+        doc.close()
+        return text
+    except fitz.FileDataError as e:
+        return None
+
+def extractInfo(text, updated_vin):
+    global year
+    global foundVIN
+    
+    if text is None:
+        print("Received None text. Skipping this VIN.")
+        # Write VIN to RETRY.txt file
+        with open(f'{year}/RETRY.txt', "a") as f:
+            f.write(f"{updated_vin}\n")
+        return
+    
+    foundVIN += 1
+    # Append only the last 6 digits of the VIN to the list and file
+    skip_corvette.append(updated_vin)
+    with open(f"{year}/skip_corvette.txt", "a") as file:
+        file.write(f"{updated_vin[-6:].zfill(6)}\n")
+
+    lines = text.split('\n')
+    info = {}
+    
+    # Define the order of fields
+    field_order = ["vin", "year", "model", "body", "trim", "engine", "transmission", "drivetrain",
+                   "exterior_color", "msrp", "dealer", "location", "ordernum", "json", "all_rpos"]
+    
+    info = {
+        "vin": updated_vin,
+        "model": "CORVETTE",
+        "drivetrain": "RWD",
+        "body": "COUPE"
+    }
+
+    for i, line in enumerate(lines):
+        if any(f"{year} {suffix}" in line for suffix in ["CORVETTE ", "COUPE CORVETTE ", "CABRIOLET CORVETTE "]):
+            model_info = ' '.join(line.strip().split())
+            modeltrim = model_info[4:].strip().split()
+            info["trim"] = ' '.join(modeltrim[1:]).replace(" CONVERTIBLE", "").replace(" COUPE", "").replace("CORVETTE ", "")
+        if "PRICE*" in line:
+            info["msrp"] = lines[i + 1].strip().replace("$","").replace(",","").replace(" ","").replace(".00","")
+        if "DELIVERED" in line:
+            json_data = ' '.join(lines[i + 7:i + 11])
+            all_json = json.loads(json_data)
+            all_json["Options"] = [option for option in all_json["Options"] if option]
+            info.update({
+                "dealer": lines[i + 1].strip().replace("\u2013", "-"),
+                "location": lines[i + 3].strip(),
+                "json": all_json,
+                "all_rpos": all_json["Options"],
+                "ordernum": all_json["order_number"],
+                "year": all_json["model_year"]
+            })
+            all_json["mmc_code"] = all_json["mmc_code"].replace(' ','')
+            all_json["sitedealer_code"] = all_json["sitedealer_code"].replace(' ','')
+
+            for item in info["all_rpos"]:
+                if item in body_dict:
+                    info["body"] = body_dict[item]
+                if item in colors_dict:
+                    info["exterior_color"] = colors_dict[item]
+                if item in engines_dict:
+                    info["engine"] = engines_dict[item]
+                if item in trans_dict:
+                    info["transmission"] = trans_dict[item]
+                if item == "HP1":
+                    info["drivetrain"] = "AWD"
+    
+    # Reorder the fields
+    info_ordered = {field: info.get(field, None) for field in field_order}
+
+    # Check for missing fields
+    missing_fields = [field for field, value in info_ordered.items() if value is None]
+    if missing_fields:
+        with open(f'{year}/missing_info.txt', "a") as f:
+            f.write(f"{updated_vin} - {','.join(missing_fields)}\n")
+    
+    return info_ordered
+
+def writeCSV(pdf_info):
+    global year
+    if pdf_info is None:
+        return
+    # Define the field names based on the keys of pdf_info
+    fieldnames = pdf_info.keys()
+    
+    # Open the CSV file in append mode with newline='' to avoid extra newline characters
+    with open(f"{year}/{year}_corvette.csv", "a", newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        # Write the pdf_info to the CSV file
+        writer.writerow(pdf_info)
+
+# Main vin processing ---------------------------------------------------------------------------
+def processVin(urlIdent, vinChanging, endVIN, yearDig):
+    global totalVIN
+    global foundVIN
+    urlFirst = "https://cws.gm.com/vs-cws/vehshop/v2/vehicle/windowsticker?vin=1G1F"
+
+    # Keep going until a specific stopping point
+    while vinChanging <= endVIN:
+        try:
+            # Build the URL (first half + identify trim/gear + check digit + year digit + 0 + incrementing VIN)
+            matchedVIN = "1G1F" + urlIdent + "X" + yearDig + "0" + str(vinChanging)
+            updated_vin = calculate_check_digit(matchedVIN)
+            newUrl = urlFirst + urlIdent + updated_vin[8:11] + str(vinChanging).zfill(6)
+
+            max_retries = 3
+            retries = 0
+
+            while retries < max_retries:
+                try:
+                    # Get Request
+                    contentsGet = requests.get(newUrl, headers = {'User-Agent': 'corvette count finder', 'Accept-Language': 'en-US'}, timeout=120)
+                    contents = contentsGet.text
+                    time.sleep(1)
+
+                    # Check if request returns errorMessage or actual content (meaning a window sticker was found)
+                    try:
+                        # If json content found = no window sticker
+                        jsonCont = json.loads(contents)
+                        print("\033[30m" + jsonCont["errorMessage"] + "\033[0m")
+                    # If request returns not a json content = window sticker found
+                    except json.decoder.JSONDecodeError:
+                        # Write VIN to txt file
+                        with open(f"{year}/corvette_{year}.txt", "a") as f:
+                            f.write(f"{updated_vin}\n")
+                        # Inform console
+                        print("\033[33mMatch Found For VIN: [" + updated_vin + "].\033[0m")
+                        pdf_text = extractPDF(contentsGet, updated_vin)
+                        pdf_info = extractInfo(pdf_text, updated_vin)
+                        writeCSV(pdf_info)
+
+                    # Increment VIN by 1
+                    vinChanging += 1
+                    totalVIN += 1
+                    break
+
+                except requests.exceptions.ReadTimeout:
+                    # Retry request
+                    print("Timed out, retrying...")
+                    retries += 1
+                    time.sleep(120)
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+            if isinstance(e, requests.exceptions.ConnectionError) and isinstance(e.__cause__, ConnectionResetError):
+                # Write VIN to RETRY.txt file
+                with open(f'{year}/RETRY.txt', "a") as f:
+                    f.write(str(updated_vin + "\n"))
+                vinChanging += 1  # Move to the next VIN
+                print("ConnectionResetError occurred. Retrying...")
+                continue  # Continue with the next VIN
+            else:
+                print("Skipping this VIN.")
+                # Write VIN to RETRY.txt file
+                with open(f'{year}/RETRY.txt', "a") as f:
+                    f.write(f"{updated_vin}\n")
+                vinChanging += 1  # Move to the next VIN
+                continue  # Continue with the next VIN
+
+        # When canceled in console, record last checked VIN to lastVin.txt
+        except KeyboardInterrupt:
+            break
+
+while True:
+    vinChanging_input = input('Enter last 6 numbers of the VIN to start at:\n')
+    if vinChanging_input.isdigit() and len(vinChanging_input) == 6:
+        vinChanging = int(vinChanging_input)
+        break
+    else:
+        print("Please enter a valid 6-digit number.")
+while True:
+    endVIN_input = input('Enter last 6 numbers of the VIN to stop at:\n')
+    if endVIN_input.isdigit() and len(endVIN_input) == 6:
+        endVIN = int(endVIN_input)
+        break
+    else:
+        print("Please enter a valid 6-digit number.")
+
+totalVIN = 0
+foundVIN = 0
+i = 1
+
+startTime = time.time()
+
+# Process request through all variations of trim/gears
+for urlIdent in chosenList:
+    urlList = len(chosenList)
+    print("Testing configuration (" + str(i) + "/" + str(urlList) + "): " + urlIdent + " -------------------------------")
+    processVin(urlIdent, vinChanging, endVIN, yearDig)
+    print("")
+    i += 1
+
+endTime = time.time()
+elapsedTime = endTime - startTime
+elapsedTime = round(elapsedTime,1)
+
+hours = int(elapsedTime // 3600)
+remainder = elapsedTime % 3600
+minutes = int(remainder // 60)
+seconds = int(remainder  % 60)
+
+with open(f'{year}/time.txt', "a") as f:
+    f.write("{},{},{}\n".format(vinChanging_input, endVIN_input, elapsedTime))
+
+t = time.localtime()
+currentTime = time.strftime("%H:%M:%S", t)
+print("Ended:", currentTime, " - Elapsed time: {} hour(s), {} minute(s), {} second(s)".format(hours, minutes, seconds))
+print("Tested {} VIN(s) - Found {} match(es)".format(totalVIN, foundVIN))
---
+++ Corvette/variables.py
@@ -0,0 +1,251 @@
+# Working Check Digit Calculator --------------------------------------------------------
+# Step 1: Assign values to letters
+alpha_numeric_conversion = {
+    'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8,
+    'J': 1, 'K': 2, 'L': 3, 'M': 4, 'N': 5, 'P': 7, 'R': 9, 'S': 2,
+    'T': 3, 'U': 4, 'V': 5, 'W': 6, 'X': 7, 'Y': 8, 'Z': 9
+}
+
+# Step 2: Position and Weight Factor
+weight_factors = [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2]
+
+colors_dict = {
+    "GAZ": "SUMMIT WHITE",
+    "GNW": "PANTHER BLACK MATTE",
+    "GLK": "PANTHER BLACK METALLIC",
+    "GBA": "BLACK",
+    "GJI": "SHADOW GRAY METALLIC",
+    "G9K": "SATIN STEEL GREY METALLIC",
+    "GXD": "SHARKSKIN METALLIC",
+    "GKK": "RIVERSIDE BLUE METALLIC",
+    "GJV": "RIPTIDE BLUE METALLIC",
+    "GMO": "RAPID BLUE",
+    "GJ0": "RALLY GREEN METALLIC",
+    "GKO": "SHOCK",
+    "GCP": "NITRO YELLOW METALLIC",
+    "GCF": "VIVID ORANGE METALLIC",
+    "G16": "CRUSH",
+    "GSK": "WILD CHERRY TINTCOAT",
+    "GNT": "RADIANT RED TINTCOAT",
+    "G7E": "GARNET RED TINTCOAT",
+    "G7C": "RED HOT"
+}
+
+engines_dict = {
+    "LT2": "6.2L V8 DI",
+    "LT6": "5.5L V8 DI",
+    "LT7": "5.5L (335 ci) Twin-Turbo V8"
+}
+
+body_dict = {
+    "CM8": "CONVERTIBLE"
+}
+
+trans_dict = {
+    "M1L": "DCT8",
+    "M1M": "DCT8",
+    "MLH": "DCT8",
+}
+
+trim_dict = {
+    "3LZ": "3LZ",
+    "2LZ": "2LZ",
+    "1LZ": "1LZ",
+    "3LT": "3LT",
+    "2LT": "2LT",
+    "1LT": "1LT"
+}
+
+# Year digit
+years = {
+    '2025': 'S',
+    '2024': 'R',
+    '2023': 'P',
+    '2022': 'N',
+    '2021': 'M',
+    '2020': 'L'
+}
+
+# Manual/Auto/Coupe/Conv. Differentiations - Ordered by 2024 Highest-Lowest
+urlIdent_list_2024 = [
+    "B1RS",
+    "H1R7",
+    "D1RS",
+    "D3DS",
+    "H3D7",
+    "F1R7",
+    "B3DS",
+    "G1R7",
+    "K1R6",
+    "J1R6",
+    "F3D7",
+    "E1R7",
+    "K3D6",
+    "G3D7",
+    "A1RS",
+    "C1RS",
+    "J3D6",
+    "C3DS",
+    "A3DS",
+    "E3D7"
+]
+
+# Manual/Auto/Coupe/Conv. Differentiations - Ordered by 2023 Highest-Lowest
+urlIdent_list_2023 = [
+    "B1RX",
+    "F1R7",
+    "H1R7",
+    "B1RS",
+    "F3D7",
+    "K1R6",
+    "B3DX",
+    "D1RS",
+    "E1R7",
+    "H3D7",
+    "D3DS",
+    "B3DS",
+    "J1R6",
+    "G1R7",
+    "K3D6",
+    "D1RX",
+    "D3DX",
+    "E3D7",
+    "A1RX",
+    "G3D7",
+    "J3D6",
+    "A1RS",
+    "C1RS",
+    "C3DS",
+    "A3DX",
+    "C3DX",
+    "C1RX",
+    "A3DS"
+]
+
+# Manual/Auto/Coupe/Conv. Differentiations - Ordered by 2022 Highest-Lowest
+urlIdent_list_2022 = [
+    "F1R7",
+    "B1RX",
+    "H1R7",
+    "E1R7",
+    "K1R6",
+    "B1RS",
+    "F3D7",
+    "G1R7",
+    "H3D7",
+    "D1RS",
+    "J1R6",
+    "B3DX",
+    "D1RX",
+    "D3DS",
+    "K3D6",
+    "A1RX",
+    "B3DS",
+    "E3D7",
+    "D3DX",
+    "G3D7",
+    "A1RS",
+    "C1RS",
+    "C1RX",
+    "J3D6",
+    "A3DX",
+    "C3DS",
+    "C3DX",
+    "A3DS"
+]
+
+# Manual/Auto/Coupe/Conv. Differentiations - Ordered by 2021 Highest-Lowest
+urlIdent_list_2021 = [
+    "F1R7",
+    "B1RS",
+    "B1RX",
+    "H1R7",
+    "E1R7",
+    "D1RS",
+    "K1R6",
+    "H3D7",
+    "B3DS",
+    "G1R7",
+    "J1R6",
+    "D3DS",
+    "F3D7",
+    "A1RX",
+    "A1RS",
+    "D1RX",
+    "K3D6",
+    "C1RS",
+    "B3DX",
+    "G3D7",
+    "D3DX",
+    "E3D7",
+    "J3D6",
+    "C3DS",
+    "A3DS",
+    "C1RX",
+    "A3DX",
+    "C3DX"
+]
+
+# Manual/Auto/Coupe/Conv. Differentiations - Ordered by 2020 Highest-Lowest
+urlIdent_list_2020 = [
+    "B1RS",
+    "B1RX",
+    "H1R7",
+    "F1R7",
+    "B3DX",
+    "H3D7",
+    "B3DS",
+    "E1R7",
+    "D1RS",
+    "K1R6",
+    "G1R7",
+    "A1RX",
+    "D3DS",
+    "J1R6",
+    "A1RS",
+    "F3D7",
+    "D1RX",
+    "K3D6",
+    "G3D7",
+    "C1RS",
+    "D3DX",
+    "E3D7",
+    "J3D6",
+    "A3DS",
+    "C1RX",
+    "C3DS",
+    "A3DX",
+    "C3DX"
+]
+
+# while True:
+#     year = input('Enter year to test:\n')
+
+#     if year in years:
+#         # yearDig = years[year]
+#         # chosenIdent = f"urlIdent_list_{year}"
+#         # chosenList = globals()[chosenIdent]
+#         break
+#     else:
+#         print("Invalid year.")
+
+# Function to calculate check digit
+def calculate_check_digit(vin):
+    total = 0
+    for i, char in enumerate(vin):
+        if char.isdigit():
+            total += int(char) * weight_factors[i]
+        elif char in alpha_numeric_conversion:
+            total += alpha_numeric_conversion[char] * weight_factors[i]
+        else:
+            raise ValueError(f"Invalid character in VIN: {char}")
+    
+    # Step 3: Divide the total by 11 and find the remainder
+    remainder = total % 11
+    
+    # Step 4: Calculate the check digit or use 'X' if remainder is 10
+    check_digit = str(remainder) if remainder < 10 else 'X'
+    
+    # Insert the check digit at the ninth position and return the updated VIN
+    updated_vin = vin[:8] + check_digit + vin[9:]
+    return updated_vin
